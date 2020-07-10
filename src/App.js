74114a84b359402a5b73dd52f232d1f841455abb
Jared Meier <meier.jared@gmail.com>
2020-07-10 17:12:05 -0400

    Add Material Design package and basic cards for stats

--- src/App.js
+++ src/App.js
@@ -3,11 +3,28 @@ import React from 'react';
 import { Cards, Chart, CountryPicker } from './components';
 import styles from './App.module.css';
 
+import { fetchData } from './api';
+
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      data: {}
+    }
+  }
+
+  async componentDidMount() {
+    const data = await fetchData();
+    this.setState({ data });
+  }
+
   render() {
+    const { data } = this.state;
+
     return (
       <div className={styles.container}>
-        <Cards />
+        <Cards data={data}/>
         <CountryPicker />
         <Chart />
       </div>
--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,9 +1,59 @@
 import React from 'react';
+import { Card, CardContent, Typography, Grid } from '@material-ui/core';
+import CountUp from 'react-countup';
 
-const Cards = () => {
-  return (
-    <h1>Cards</h1>
+import styles from './Cards.module.css';
+
+const Cards = ({ data: { confirmed, recovered, deaths, lastUpdate } }) => {
+  if (!confirmed) return (
+    <h1>Loading...</h1>
   )
+
+  return (
+    <div className={styles.container}>
+      <Grid container spacing={3} justify="center">
+        <Grid item component={Card}>
+          <CardContent>
+            <Typography color="textSecondary" gutterBottom>
+              Infected
+            </Typography>
+            <Typography variant="h5">
+              <CountUp 
+                start={0}
+                end={confirmed.value}
+                duration={2.5}
+                separator=","
+              />
+            </Typography>
+            <Typography color="textSecondary">*REAL DATE*</Typography>
+            <Typography variant="body2">Number of active cases</Typography>
+          </CardContent>
+        </Grid>
+
+        <Grid item component={Card}>
+          <CardContent>
+            <Typography color="textSecondary" gutterBottom>
+              Infected
+            </Typography>
+            <Typography variant="h5">{recovered.value}</Typography>
+            <Typography color="textSecondary">*REAL DATE*</Typography>
+            <Typography variant="body2">Number of recoveries</Typography>
+          </CardContent>
+        </Grid>
+
+        <Grid item component={Card}>
+          <CardContent>
+            <Typography color="textSecondary" gutterBottom>
+              Infected
+            </Typography>
+            <Typography variant="h5">{deaths.value}</Typography>
+            <Typography color="textSecondary">*REAL DATE*</Typography>
+            <Typography variant="body2">Number of deaths</Typography>
+          </CardContent>
+        </Grid>
+      </Grid>
+    </div>
+  );
 }
 
 export default Cards;
